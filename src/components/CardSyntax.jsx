8ffd6db4eef43bbf2962bf33364a7f6edcadc36b
Yerovy Espitia <luyeesgar@gmail.com>
2021-09-27 08:42:21 -0500

    third desing almost done

--- src/components/CardSyntax.jsx
+++ src/components/CardSyntax.jsx
@@ -7,18 +7,38 @@ import { search } from "../redux/user";
 //CardSyntax toma la informacón de Cards, la organiza y la muestra
 
 const CardSyntax = ({ pokemon }) => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const type = pokemon.types[0].type.name;
   const type2 = pokemon.types.length > 1 ? pokemon.types[1].type.name : "";
 
   const handleOnClick = () => {
-    dispatch(search({ searchTerm: "" }))
+    dispatch(search({ searchTerm: "" }));
   };
 
   return (
     <div className="pokemon" onClick={handleOnClick}>
       <Link to={`/view/${pokemon.name}`}>
-        <h2 className="title">{pokemon.name}</h2>
+        <div className="pokemon-container">
+          <div className="background-container">
+            <div className="pokemon-img-container">
+              <img
+                src={pokemon.sprites.other["official-artwork"].front_default}
+                alt={pokemon.name}
+                title={pokemon.name}
+              />
+            </div>
+            <h2 className="title">{pokemon.name}</h2>
+            <div className="pokemon-types">
+              <p className={`types ${type}`}>{type}</p>
+              {pokemon.types.length > 1 ? (
+                <p className={`types ${type2}`}>{pokemon.types[1].type.name}</p>
+              ) : (
+                <span></span>
+              )}
+            </div>
+          </div>
+        </div>
+        {/* <h2 className="title">{pokemon.name}</h2>
         <div className="pokemon-img-container">
           <img
             src={pokemon.sprites.other["official-artwork"].front_default}
@@ -34,7 +54,7 @@ const CardSyntax = ({ pokemon }) => {
           ) : (
             <span></span>
           )}
-        </div>
+        </div> */}
       </Link>
     </div>
   );
